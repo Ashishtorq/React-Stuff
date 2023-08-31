c4292d6e34b97deff22e486efcea0edec41c1d2b
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-01 00:36:51 +0530

    bg changer

--- bgchanger/src/App.jsx
+++ bgchanger/src/App.jsx
@@ -1,13 +1,49 @@
-import { Fragment } from "react"
-import { useState } from "react"
+import { Fragment } from "react";
+import { useState } from "react";
 
 function App() {
-
+  const [color, setcolor] = useState("#99CA63");
   return (
     <Fragment>
-      
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "red" }}
+              onClick={() => setcolor("red")}
+            >
+              Red
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "blue" }}
+              onClick={() => setcolor("blue")}
+            >
+              Blue
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "green" }}
+              onClick={() => setcolor("green")}
+            >
+              Green
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "yellow" }}
+              onClick={() => setcolor("yellow")}
+            >
+              Yellow
+            </button>
+          </div>
+        </div>
+      </div>
     </Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
