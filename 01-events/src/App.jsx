1a4d942368299feb86f56b11c9726736cc8a12e0
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-03 04:09:50 +0530

    props

--- 01-events/src/App.jsx
+++ 01-events/src/App.jsx
@@ -1,16 +1,37 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import Student from "./Student";
 const App = () => {
-  const [name,setName] = useState("ashish");
-  function data (){
-    setName("Singh");
+  const [name, setName] = useState(0);
+  function data() {
+    setName(name + 1);
   }
   return (
     <div>
       <h1>{name}</h1>
       <button onClick={data}>{name}</button>
+      <Student
+        name={"Ashish"}
+        sirname={"Singh"}
+        address={"Crossing Republic gol chakkar"}
+      ></Student>
+      <Student
+        name={"Yashwant"}
+        sirname={"Singh"}
+        address={"Crossing Republic gol chakkar"}
+      ></Student>
+      <Student
+        name={"Sharma"}
+        sirname={"Singh"}
+        address={"Crossing Republic gol chakkar"}
+      ></Student>
+      <Student
+        name={"ABD"}
+        sirname={"Singh"}
+        address={"Crossing Republic gol chakkar"}
+      ></Student>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
