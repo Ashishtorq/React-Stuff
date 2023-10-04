7a331591c3498ee57a50c17db973e7d1429cb14b
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-05 00:41:29 +0530

    map in react

--- 01-events/src/App.jsx
+++ 01-events/src/App.jsx
@@ -3,19 +3,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Student from "./Student";
 import Button from "react-bootstrap/Button";
 import './App.css'
-const App = () => {
-  const [name, setName] = useState(100);
-  const [datas, setData] = useState(0);
-  useEffect(()=>{
-    console.warn("This is me");
-  },[datas])
+ const App = () => {
+//   const [name, setName] = useState(100);
+//   const [datas, setData] = useState(0);
+//   useEffect(()=>{
+//     console.warn("This is me");
+//   },[datas])
+const st = ['ashish','anil','singh'];
+st.map((e)=>{
+  console.warn("Name : ", e);
+})
+for(let i=0; i<st.length; i++){
+  console.warn("Name in loop : ",st[i])
+}
   return (
     <Fragment>
-      <h1 className="data">Datas : {datas}</h1>
-      <h1 className="data">name : {name}</h1>
-      <Button onClick={()=>setName(name-1)} variant="outline-primary">++</Button>{" "}
-      <Button onClick={()=>setData(datas+1)} variant="primary">---</Button>{" "}
-      
+     
     </Fragment>
   );
 };
