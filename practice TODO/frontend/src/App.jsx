5c6b5eb157158425dd15b874d4875fd215ebb71f
Ashistorq <ashishsinghtarwa@gmail.com>
2023-10-21 11:51:37 +0530

    component

--- practice TODO/frontend/src/App.jsx
+++ practice TODO/frontend/src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from "react";
 import "./App.css";
-
+import { fakeData } from "./data";
 function App() {
-  const [count, setCount] = useState(0);
-
+ const [data, setdata] = useState(fakeData);
   return (
-    <div>
-      <h1>To Do List</h1>
-      <form className="form">
-        <input type="text" placeholder="Enter your Goal" />
-        <button type="submit">Add Task</button>
-      </form>
-      <h2>Tasks To Do</h2>
-      <ul>
-        <li>Data Structure</li>
-        <li>OOPS</li>
-        <li>DAA</li>
-      </ul>
+    <div className="card">
+      <div>
+        <h1>To Do List</h1>
+        <form className="form">
+          <input type="text" placeholder="Enter your Goal" />
+          <button type="submit">Add Task</button>
+        </form>
+        <h2>Tasks To Do</h2>
+        <ul>
+          {
+            data.map((d)=>{
+              return (
+                <li key={d.id} >{d.data}</li>
+              )
+            })
+          }
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ practice TODO/frontend/src/data.js
@@ -0,0 +1,6 @@
+export  const fakedata = [
+  { id: 1, data: "Data Structure" },
+  { id: 2, data: "Object Oriented" },
+  { id: 1, data: "System Design" },
+  { id: 2, data: "High level Design" },
+];
